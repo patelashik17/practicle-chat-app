fc4c348a3a83f8c8f3b4afc56d22208e304bd120
Patel Ashik <patelashik2226@gmail.com>
2026-08-22 14:23:56 +0530

    Remove jest.config.ts file as part of project restructuring.

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,5 @@
-import type { Config } from "jest";
-
-const config: Config = {
+/** @type {import('jest').Config} */
+module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   roots: ["<rootDir>/tests"],
@@ -8,5 +7,3 @@ const config: Config = {
   testMatch: ["**/*.test.ts"],
   clearMocks: true,
 };
-
-export default config;
